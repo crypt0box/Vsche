cf0f5415e44535eb973735093bebf1446007ea90
crypt0box <pray.happy76@gmail.com>
2021-02-23 16:45:15 +0900

    Improve

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 }).then((responses) => {
                     if (responses[0].queryResult && responses[0].queryResult.action == "handle-delivery-order"){
                         let message_text
-                        if (responses[0].queryResult.parameters.fields.menu.stringValue){
-                            message_text = `毎度！${responses[0].queryResult.parameters.fields.menu.stringValue}ね。どちらにお届けしましょ？`;
+                        if (responses[0].queryResult.parameters.fields.schedule.stringValue){
+                            message_text = `毎度！${responses[0].queryResult.parameters.fields.schedule.stringValue}ね。どちらにお届けしましょ？`;
                         } else {
                             message_text = `毎度！ご注文は？`;
                         }
