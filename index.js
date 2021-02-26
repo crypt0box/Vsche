a373603a8441c62cd0be7b267bdadc578d4cc3ee
crypt0box <pray.happy76@gmail.com>
2021-02-26 21:22:35 +0900

    Improve

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                             .then(result => {
                               const videoId = result.data.items[0].id.videoId
                               streamingUrl = "https://www.youtube.com/watch?v=" + videoId;
+                              console.log(streamingUrl);
                             })
                         } 
                         return bot.replyMessage(event.replyToken, {
