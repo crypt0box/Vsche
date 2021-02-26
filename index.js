f2378c79d17427ed4e980e672ef82038a63030bc
crypt0box <pray.happy76@gmail.com>
2021-02-26 22:08:49 +0900

    Improve

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    ).catch(error => {
-      console.log(error);
-    });
+    // Promise.all(events_processed).then(
+    //     (response) => {
+    //         console.log(`${response.length} event(s) processed.`);
+    //     }
+    // ).catch(error => {
+    //   console.log(error);
+    // });
 });
