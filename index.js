96f4c8764aa4d326fa6e3bf722947cdfb4a9dd02
crypt0box <pray.happy76@gmail.com>
2021-02-28 15:14:01 +0900

    UPDATE: 現在時刻より後の配信予定を取得するようにする

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 async function fetchStreamingSummary(channelId) {
   try {
-    const apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channelId + "&key=" + YOUTUBE_API_KEY + "&eventType=upcoming&type=video";
+    const now = new Date();
+    const apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channelId + "&key=" + YOUTUBE_API_KEY + "&eventType=upcoming&type=video&publishedAfter=" + now;
     const response = await axios.get(apiUrl);
     return response;
   } catch (error) {
